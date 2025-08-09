b697eb9f3e4c8fd68075d0fd7e7409b89d7a5a95
codx_venu <vasulallu09@gmail.com>
2025-08-10 00:23:04 +0530

    push

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -4,12 +4,10 @@ function checkout() {
   return (
     <div>
       <Nav/>
-      <div className="main-checkout-menu grid grid-cols-2">
-
-     
-      <div className="personal_details pl-[159px] overflow-y-auto">
+      <div className="main-checkout-menu flex justify-center ">
+      <div className="personal_details w-1/2 pt-[6rem] overflow-y-auto">
        
-<div className="bg-[var(--background)] text-[var(--border)] shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 border-r-[0.25px] border-[#121212]">
+<div className="bg-[var(--background)] text-[var(--border)] shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 border-[1px] border-[whitesmoke]">
 <h1 className='mb-5 text-[18px] text-[var(--text)]' >Personal Details</h1>
   <div className="-mx-3 md:flex mb-6">
     <div className="md:w-1/2 px-3 mb-6 md:mb-0">
@@ -84,60 +82,21 @@ function checkout() {
   
   <h1 className='mt-2 text-[18px] text-[var(--text)]'> Payment</h1>
   <small className='text-[var(--border)]'>All transactions are secure and encrypted.</small>
-  <div className='border-[1px] border-[var(--border)] border-t-[#5f91ed] rounded'>
-    <span className='border-[2px] border-[#5f91ed] flex mb-2 p-4'>
+  <div className='border-[1px] border-[var(--border)] rounded'>
+    <span className='border-[2px]  flex mb-2 p-4'>
 
     <h1 className='flex gap-2'>Razorpay Secure (UPI, Cards, Wallets, NetBanking) <div className="flex"><img alt="upi" src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/c1.en/assets/upi.CmgCfll8.svg" role="img" width="38" height="24" /><img alt="visa" src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/c1.en/assets/visa.sxIq5Dot.svg" role="img" width="38" height="24" /><img alt="master" src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/c1.en/assets/master.CzeoQWmc.svg" role="img" width="38" height="24" /><div ><img alt="netbanking" src="https://cdn.shopify.com/shopifycloud/checkout-web/assets/c1.en/assets/netbanking.C9e9yzjv.svg" role="img" width="38" height="24" /></div></div></h1>
     </span>
     <i className='flex justify-center mt-4'><svg xmlns="http://www.w3.org/2000/svg" viewBox="-252.3 356.1 163 80.9" className="w-[163px] "><path fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2" d="M-108.9 404.1v30c0 1.1-.9 2-2 2H-231c-1.1 0-2-.9-2-2v-75c0-1.1.9-2 2-2h120.1c1.1 0 2 .9 2 2v37m-124.1-29h124.1"></path><circle cx="-227.8" cy="361.9" r="1.8" fill="var(--text)"></circle><circle cx="-222.2" cy="361.9" r="1.8" fill="var(--text)"></circle><circle cx="-216.6" cy="361.9" r="1.8" fill="var(--text)"></circle><path fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2" d="M-128.7 400.1H-92m-3.6-4.1 4 4.1-4 4.1"></path></svg></i>
     <p className='w-[58%] text-center m-auto mt-5 mb-4'>After clicking “Pay now”, you will be redirected to Razorpay Secure (UPI, Cards, Wallets, NetBanking) to complete your purchase securely.</p>
   </div>
-  <button className="paynow bg-blue-400 text-[18px] font-extrabold  py-2 mt-9 rounded w-[100%]">
+  <button className="paynow bg-blue-400 text-[18px] font-extrabold  text-white py-2 mt-9 rounded w-[100%]">
     Pay Now
   </button>
   </div>
 </div> 
 
 </div>
-
-
-<div className='bg-[#0b0a0a] p-[38px] text-[var(--text)] sticky'> 
-    <span className='mt-5 flex text-[var(--text)] gap-4 items-center w-[410px]' >
-
-    <img className='w-[64px] rounded' src="https://www.punkstore.in/cdn/shop/files/A2644816-EC12-48CD-9F9B-02B9FA0CC0E2.jpg?v=1738213874&width=600" alt="" />
-    <h1 className='text-[17px]'>Tissot punk green</h1> <small className='ml-auto text-[18px]'>
-    ₹8,100.00</small>
-    </span>
-    <span className='mt-5 flex text-[var(--text)] gap-4 items-center w-[410px]' >
-
-    <img className='w-[64px] rounded' src="https://www.punkstore.in/cdn/shop/files/A2644816-EC12-48CD-9F9B-02B9FA0CC0E2.jpg?v=1738213874&width=600" alt="" />
-    <h1 className='text-[17px]'>Tissot punk green</h1> <small className='ml-auto text-[18px]'>
-    ₹8,100.00</small>
-    </span>
-    <span className='mt-5 flex text-[var(--text)] gap-4 items-center w-[410px]' >
-
-    <img className='w-[64px] rounded' src="https://www.punkstore.in/cdn/shop/files/A2644816-EC12-48CD-9F9B-02B9FA0CC0E2.jpg?v=1738213874&width=600" alt="" />
-    <h1 className='text-[17px]'>Tissot punk green</h1> <small className='ml-auto text-[18px]'>
-    ₹8,100.00</small>
-    </span>
-    <span className='mt-5 flex gap-5 mb-5'>
-
-   <input type="text" placeholder='Discount Code'  className='w-[320px] rounded bg-transparent border-[1px] text-[var(--border)] p-2'/>
-   <button className="apply rounded bg-[#3885ff] py-3 px-5">Apply</button>
-    </span>
-    <div className="context-checkout grid grid-cols-2 grid-rows-4">
-        <h1 className='text-[17px] font-bold'>Subtotal · 5 items</h1>
-        <h1 className='text-[17px] font-bold'>₹22,700.00</h1>
-        <h2 className='text-[17px] font-bold'>Shipping</h2>
-        <h2 className='text-[17px] font-bold text-[var(--border)]'>Enter shipping address</h2>
-        <h3 className='text-[22px] font-extrabold'>Total</h3>
-        <h3 className='font-extrabold text-[22px]'><span className='font-normal text-[14px] text-[var(--border)]'>INR</span>
-₹22,700.00
-
-</h3>
-<small className='text-[15px] font-bold text-[var(--border)]'>Including ₹538.10 in taxes</small>
-    </div>
-    </div>
 </div>
       </div>
 
